9022bab3df13100bd661b000997cc39dc624917e
Murtuza Quaizar <MQuaizar@RJBRANDS.local>
2023-02-26 17:20:27 -0500

    project creation with code composer

---
+++ blink.c
@@ -0,0 +1,22 @@
+#include <msp430.h>				
+
+
+/**
+ * blink.c
+ */
+void main(void)
+{
+	WDTCTL = WDTPW | WDTHOLD;		// stop watchdog timer
+	P1DIR |= 0x01;					// configure P1.0 as output
+	P1DIR |= 0x02;                  // configure P1.1 as output
+
+	P1OUT |= 0x02;              // toggle P1.0
+
+	volatile unsigned int i;		// volatile to prevent optimization
+
+	while(1)
+	{
+		P1OUT ^= 0x01;				// toggle P1.0
+		for(i=10000; i>0; i--);     // delay
+	}
+}
